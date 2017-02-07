05ce91bad4fde3eb15b9d5738a18ea0a3349f9d4
resakemal <resakemal@gmail.com>
2017-02-07 13:01:23 +0700

    Ledakan & Pergerakan Pecahan Pesawat
    Prosedur membuat ledakan dan prosedur pergerakan pecahan pada garis
    lurus

--- 3.cpp
+++ 3.cpp
@@ -3,72 +3,229 @@ using namespace std;
 
 FrameBuffer *fb;
 
-void planePart1(float x, float y, int r, int g, int b) { //moncong pesawat; x,y sebagai pusat
-	fb->draw_line(x-8,y-3,x+8,y-11,0,0,150);
-	fb->draw_line(x-8,y-3,x-8,y+3,0,0,150);
-	fb->draw_line(x-8,y+3,x+8,y+11,0,0,150);
-	fb->draw_line(x+8,y-11,x+8,y+11,0,0,150);
-	fb->flood_fill(x,y,r,g,b);
+void drawExplosion(float xc, float yc){		
+	//Inner explosion
+	fb->draw_line(xc+15,yc+20,xc+50,yc+40,255,255,0); //1
+	fb->draw_line(xc+50,yc+40,xc+20,yc+10,255,255,0); //2
+	fb->draw_line(xc+20,yc+10,xc+40,yc,255,255,0); //3
+	fb->draw_line(xc+40,yc,xc+20,yc-10,255,255,0); //4
+	fb->draw_line(xc+20,yc-10,xc+25,yc-40,255,255,0); //5
+	fb->draw_line(xc+25,yc-40,xc,yc-15,255,255,0); //6
+	fb->draw_line(xc,yc-15,xc-10,yc-30,255,255,0); //7
+	fb->draw_line(xc-10,yc-30,xc-20,yc-5,255,255,0); //8
+	fb->draw_line(xc-20,yc-5,xc-45,yc-13,255,255,0); //9
+	fb->draw_line(xc-45,yc-13,xc-30,yc+7,255,255,0); //10
+	fb->draw_line(xc-30,yc+7,xc-55,yc+15,255,255,0); //11
+	fb->draw_line(xc-55,yc+15,xc-14,yc+23,255,255,0); //12
+	fb->draw_line(xc-14,yc+23,xc-2,yc+38,255,255,0); //13
+	fb->draw_line(xc-2,yc+38,xc+15,yc+20,255,255,0); //14
+	
+	//jeda untuk ledakan
+	usleep(300000);
+	
+	//Middle explosion
+	fb->draw_line(xc+20,yc+40,xc+80,yc+65,255,165,0); //1
+	fb->draw_line(xc+80,yc+65,xc+45,yc+12,255,165,0); //2
+	fb->draw_line(xc+45,yc+12,xc+63,yc-5,255,165,0); //3
+	fb->draw_line(xc+63,yc-5,xc+35,yc-17,255,165,0); //4
+	fb->draw_line(xc+35,yc-17,xc+40,yc-60,255,165,0); //5
+	fb->draw_line(xc+40,yc-60,xc,yc-32,255,165,0); //6
+	fb->draw_line(xc,yc-32,xc-13,yc-43,255,165,0); //7
+	fb->draw_line(xc-13,yc-43,xc-25,yc-17,255,165,0); //8
+	fb->draw_line(xc-25,yc-17,xc-65,yc-26,255,165,0); //9
+	fb->draw_line(xc-65,yc-26,xc-52,yc+4,255,165,0); //10
+	fb->draw_line(xc-52,yc+4,xc-70,yc+15,255,165,0); //11
+	fb->draw_line(xc-70,yc+15,xc-18,yc+39,255,165,0); //12
+	fb->draw_line(xc-18,yc+39,xc-8,yc+57,255,165,0); //13
+	fb->draw_line(xc-8,yc+57,xc+20,yc+40,255,165,0); //14
+	
+	//jeda untuk ledakan
+	usleep(300000);
+	
+	//Outer explosion
+	fb->draw_line(xc+25,yc+60,xc+110,yc+95,255,0,0); //1
+	fb->draw_line(xc+110,yc+95,xc+60,yc+10,255,0,0); //2
+	fb->draw_line(xc+60,yc+10,xc+85,yc-10,255,0,0); //3
+	fb->draw_line(xc+85,yc-10,xc+45,yc-22,255,0,0); //4
+	fb->draw_line(xc+45,yc-22,xc+52,yc-72,255,0,0); //5
+	fb->draw_line(xc+52,yc-72,xc,yc-48,255,0,0); //6
+	fb->draw_line(xc,yc-48,xc-18,yc-62,255,0,0); //7
+	fb->draw_line(xc-18,yc-62,xc-34,yc-24,255,0,0); //8
+	fb->draw_line(xc-34,yc-24,xc-80,yc-38,255,0,0); //9
+	fb->draw_line(xc-80,yc-38,xc-65,yc+4,255,0,0); //10
+	fb->draw_line(xc-65,yc+4,xc-87,yc+18,255,0,0); //11
+	fb->draw_line(xc-87,yc+18,xc-20,yc+45,255,0,0); //12
+	fb->draw_line(xc-20,yc+45,xc-10,yc+70,255,0,0); //13
+	fb->draw_line(xc-10,yc+70,xc+25,yc+60,255,0,0); //14
 }
 
-void planePart2(float x, float y, int r, int g, int b) { //badan depan pesawat; x,y sebagai pusat
-	fb->draw_line(x-32,y-11,x+33,y-11,0,0,150);
-	fb->draw_line(x-32,y+11,x+33,y+11,0,0,150);
-	fb->draw_line(x-32,y-11,x-32,y+11,0,0,150);
-	fb->draw_line(x+33,y-11,x+33,y+11,0,0,150);
-	fb->flood_fill(x,y,r,g,b);
+void selectPart(float x, float y, int r, int g, int b, int num, bool reset) {
+	int bbor;
+	// Menghapus instance pecahan sebelumnya
+	if (reset) {
+		bbor = 0;
+	}
+	else {
+		bbor = 150;
+	}
+	
+	switch(num) {
+		case 1: { //moncong pesawat; x,y sebagai pusat
+			fb->draw_line(x-8,y-3,x+8,y-11,0,0,bbor);
+			fb->draw_line(x-8,y-3,x-8,y+3,0,0,bbor);
+			fb->draw_line(x-8,y+3,x+8,y+11,0,0,bbor);
+			fb->draw_line(x+8,y-11,x+8,y+11,0,0,bbor);
+			if (!reset) fb->flood_fill(x,y,r,g,b);
+			break;
+		}
+		case 2: { //badan depan pesawat; x,y sebagai pusat
+			fb->draw_line(x-32,y-11,x+33,y-11,0,0,bbor);
+			fb->draw_line(x-32,y+11,x+33,y+11,0,0,bbor);
+			fb->draw_line(x-32,y-11,x-32,y+11,0,0,bbor);
+			fb->draw_line(x+33,y-11,x+33,y+11,0,0,bbor);
+			if (!reset) fb->flood_fill(x,y,r,g,b);
+			break;
+		}
+		case 3: { //sayap atas; x,y sebagai titik kiri bawah sayap
+			fb->draw_line(x,y,x+44,y-46,0,0,bbor);
+			fb->draw_line(x+44,y-46,x+53,y-46,0,0,bbor);
+			fb->draw_line(x+53,y-46,x+29,y-10,0,0,bbor);
+			fb->draw_line(x+29,y-10,x+31,y,0,0,bbor);
+			fb->draw_line(x+31,y,x,y,0,0,bbor);
+			if (!reset) fb->flood_fill(x+15,y-5,r,g,b);
+			break;
+		}
+		case 4: { //sayap bawah; x,y sebagai titik kiri atas sayap
+			fb->draw_line(x,y,x+44,y+46,0,0,bbor);
+			fb->draw_line(x+44,y+46,x+53,y+46,0,0,bbor);
+			fb->draw_line(x+53,y+46,x+29,y+10,0,0,bbor);
+			fb->draw_line(x+29,y+10,x+31,y,0,0,bbor);
+			fb->draw_line(x+31,y,x,y,0,0,bbor);
+			if (!reset) fb->flood_fill(x+15,y+5,r,g,b);
+			break;
+		}
+		case 5: { //badan tengah pesawat(diapit sayap); x,y sebagai pusat
+			fb->draw_line(x-15,y-11,x+16,y-11,0,0,bbor);
+			fb->draw_line(x-15,y+11,x+16,y+11,0,0,bbor);
+			fb->draw_line(x-15,y-11,x-15,y+11,0,0,bbor);
+			fb->draw_line(x+16,y-11,x+16,y+11,0,0,bbor);
+			if (!reset) fb->flood_fill(x,y,r,g,b);
+			break;
+		}
+		case 6: { //badan belakang pesawat (diapit ekor); x,y sebagai pertengahan antara O dan H
+			fb->draw_line(x,y-9,x+15,y-11,0,0,bbor);
+			fb->draw_line(x+15,y-11,x+15,y+11,0,0,bbor);
+			fb->draw_line(x+15,y+11,x,y+9,0,0,bbor);
+			fb->draw_line(x,y+9,x-42,y+11,0,0,bbor);
+			fb->draw_line(x-42,y+11,x-42,y-11,0,0,bbor);
+			fb->draw_line(x-42,y-11,x,y-9,0,0,bbor);
+			if (!reset) fb->flood_fill(x,y,r,g,b);
+			break;
+		}
+		case 7: { //ekor atas ; x,y sebagai titik kiri bawah ekor
+			fb->draw_line(x,y,x+13,y-19,0,0,bbor);
+			fb->draw_line(x+13,y-19,x+24,y-19,0,0,bbor);
+			fb->draw_line(x+24,y-19,x+17,y-2,0,0,bbor);
+			fb->draw_line(x+17,y-2,x,y,0,0,bbor);
+			if (!reset) fb->flood_fill(x+7,y-5,r,g,b);
+			break;
+		}
+		case 8: { //ekor bawah ; x,y sebagai titik kiri atas ekor
+			fb->draw_line(x,y,x+13,y+19,0,0,bbor);
+			fb->draw_line(x+13,y+19,x+24,y+19,0,0,bbor);
+			fb->draw_line(x+24,y+19,x+17,y+2,0,0,bbor);
+			fb->draw_line(x+17,y+2,x,y,0,0,bbor);
+			if (!reset) fb->flood_fill(x+7,y+5,r,g,b);
+			break;
+		}
+	}
 }
 
-void planePart3(float x, float y, int r, int g, int b) { //sayap atas; x,y sebagai titik kiri bawah sayap
-	fb->draw_line(x,y,x+44,y-46,0,0,150);
-	fb->draw_line(x+44,y-46,x+53,y-46,0,0,150);
-	fb->draw_line(x+53,y-46,x+29,y-10,0,0,150);
-	fb->draw_line(x+29,y-10,x+31,y,0,0,150);
-	fb->draw_line(x+31,y,x,y,0,0,150);
-	fb->flood_fill(x+15,y-5,r,g,b);
-}
+void drawPecahan(float x1, float y1,float x2, float y2, int r, int g, int b, int num)
+{
+	// Bresenham's line algorithm
+	const bool steep = (fabs(y2 - y1) > fabs(x2 - x1));
+	if(steep){
+	  std::swap(x1, y1);
+	  std::swap(x2, y2);
+	}
 
-void planePart4(float x, float y, int r, int g, int b) { //sayap bawah; x,y sebagai titik kiri atas sayap
-	fb->draw_line(x,y,x+44,y+46,0,0,150);
-	fb->draw_line(x+44,y+46,x+53,y+46,0,0,150);
-	fb->draw_line(x+53,y+46,x+29,y+10,0,0,150);
-	fb->draw_line(x+29,y+10,x+31,y,0,0,150);
-	fb->draw_line(x+31,y,x,y,0,0,150);
-	fb->flood_fill(x+15,y+5,r,g,b);
-}
+	if(x1 > x2) {
+	  std::swap(x1, x2);
+	  std::swap(y1, y2);
+	}
 
-void planePart5(float x, float y, int r, int g, int b) { //badan tengah pesawat(diapit sayap); x,y sebagai pusat
-	fb->draw_line(x-15,y-11,x+16,y-11,0,0,150);
-	fb->draw_line(x-15,y+11,x+16,y+11,0,0,150);
-	fb->draw_line(x-15,y-11,x-15,y+11,0,0,150);
-	fb->draw_line(x+16,y-11,x+16,y+11,0,0,150);
-	fb->flood_fill(x,y,r,g,b);
-}
+	const float dx = x2 - x1;
+	const float dy = fabs(y2 - y1);
 
-void planePart6(float x, float y, int r, int g, int b) { //badan belakang pesawat (diapit ekor); x,y sebagai pertengahan antara O dan H
-	fb->draw_line(x,y-9,x+15,y-11,0,0,150);
-	fb->draw_line(x+15,y-11,x+15,y+11,0,0,150);
-	fb->draw_line(x+15,y+11,x,y+9,0,0,150);
-	fb->draw_line(x,y+9,x-42,y+11,0,0,150);
-	fb->draw_line(x-42,y+11,x-42,y-11,0,0,150);
-	fb->draw_line(x-42,y-11,x,y-9,0,0,150);
-	fb->flood_fill(x,y,r,g,b);
-}
+	float error = dx / 2.0f;
+	const int ystep = (y1 < y2) ? 1 : -1;
+	int y = (int)y1;
 
-void planePart7(float x, float y, int r, int g, int b) { //ekor atas ; x,y sebagai titik kiri bawah ekor
-	fb->draw_line(x,y,x+13,y-19,0,0,150);
-	fb->draw_line(x+13,y-19,x+24,y-19,0,0,150);
-	fb->draw_line(x+24,y-19,x+17,y-2,0,0,150);
-	fb->draw_line(x+17,y-2,x,y,0,0,150);
-	fb->flood_fill(x+7,y-5,r,g,b);
-}
+	const int maxX = (int)x2;
 
-void planePart8(float x, float y, int r, int g, int b) { //ekor bawah ; x,y sebagai titik kiri atas ekor
-	fb->draw_line(x,y,x+13,y+19,0,0,150);
-	fb->draw_line(x+13,y+19,x+24,y+19,0,0,150);
-	fb->draw_line(x+24,y+19,x+17,y+2,0,0,150);
-	fb->draw_line(x+17,y+2,x,y,0,0,150);
-	fb->flood_fill(x+7,y+5,r,g,b);
+	for(int x=(int)x1; x<maxX; x++) {
+	  if(steep) {
+		  selectPart(y,x,r,g,b,num,0);
+		  usleep(10000);
+		  switch(num) {
+			  case 3: {
+				fb->reset_fill(x+15,y-5,0,0,0);
+				break;
+				}
+			  case 4: {
+				fb->reset_fill(x+15,y+5,0,0,0);
+				break;
+				}
+			  case 7: {
+				fb->reset_fill(x+7,y-5,0,0,0);
+				break;
+				}
+			  case 8: {
+				fb->reset_fill(x+7,y+5,0,0,0);
+				break;
+				}
+			  default: {
+				fb->reset_fill(x,y,0,0,0);
+				}
+		  }
+		  usleep(10000);
+		  selectPart(y,x,r,g,b,num,1);
+	  }
+	  else {
+		  selectPart(x,y,r,g,b,num,0);
+		  usleep(10000);
+		  switch(num) {
+			  case 3: {
+				fb->reset_fill(x+15,y-5,0,0,0);
+				break;
+				}
+			  case 4: {
+				fb->reset_fill(x+15,y+5,0,0,0);
+				break;
+				}
+			  case 7: {
+				fb->reset_fill(x+7,y-5,0,0,0);
+				break;
+				}
+			  case 8: {
+				fb->reset_fill(x+7,y+5,0,0,0);
+				break;
+				}
+			  default: {
+				fb->reset_fill(x,y,0,0,0);
+				}
+		  }
+		  usleep(10000);
+		  selectPart(x,y,r,g,b,num,1);
+	  }
+
+	  error -= dy;
+	  if(error < 0) {
+		  y += ystep;
+		  error += dx;
+	  }
+	}
 }
 
 int main() {
@@ -123,6 +280,8 @@ int main() {
 	// printf("%i\n", fb->isBlack(100,100));
 	// printf("%i\n", fb->isBlack(400,500));
 
+	//selectPart(500,500,0,255,0,8,0);
+	//drawPecahan(200,200,500,500,0,255,0,1);
 	delete fb;
 	return 0;
 }
