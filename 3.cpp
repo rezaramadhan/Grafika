7aa41e2e7355fb8f28be5dc2b156bd76785dd3f8
Jovian Christianto <Jovian Christianto>
2017-02-07 17:48:14 +0700

    pesawat gerak, warna gradasi

--- 3.cpp
+++ 3.cpp
@@ -8,29 +8,49 @@ int main() {
 	int deltay = 60 ;
 
 	//GAMBAR PESAWAT
-	fb->draw_line(0,3+deltay,16, 11+deltay,0,0,150);
-	fb->draw_line(16,11+deltay,81,11+deltay,0,0,150);
-	fb->draw_line(81,11+deltay,125,57+deltay,0,0,150);
-	fb->draw_line(125,57+deltay,134,57+deltay,0,0,150);
-	fb->draw_line(134,57+deltay,110,21+deltay,0,0,150);
-	fb->draw_line(110,21+deltay,112,11+deltay,0,0,150);
-	fb->draw_line(112,11+deltay,154,9+deltay,0,0,150);
-	fb->draw_line(154,9+deltay,167,28+deltay,0,0,150);
-	fb->draw_line(167,28+deltay,176,28+deltay,0,0,150);
-	fb->draw_line(176,28+deltay,169,11+deltay,0,0,150);
-	fb->draw_line(169,11+deltay,169,-11+deltay,0,0,150);
-	fb->draw_line(169,-11+deltay,176,-28+deltay,0,0,150);
-	fb->draw_line(176,-28+deltay,167,-28+deltay,0,0,150);
-	fb->draw_line(167,-28+deltay,154,-9+deltay,0,0,150);
-	fb->draw_line(154,-9+deltay,112,-11+deltay,0,0,150);
-	fb->draw_line(112,-11+deltay,110,-21+deltay,0,0,150);
-	fb->draw_line(110,-21+deltay,134,-57+deltay,0,0,150);
-	fb->draw_line(134,-57+deltay,125,-57+deltay,0,0,150);
-	fb->draw_line(125,-57+deltay,81,-11+deltay,0,0,150);
-	fb->draw_line(81,-11+deltay,16,-11+deltay,0,0,150);
-	fb->draw_line(16,-11+deltay,0,-3+deltay,0,0,150);
+	
+	int deltax = 1100;
+	int n = 0;
 
-	fb->flood_fill(90,deltay,0,0,150);
+	while (n < 3) 
+	{
+		fb->draw_line(deltax+0,3+deltay,deltax+16, 11+deltay,0,0,150);
+		fb->draw_line(deltax+16,11+deltay,deltax+81,11+deltay,0,0,150);
+		fb->draw_line(deltax+81,11+deltay,deltax+125,57+deltay,0,0,150);
+		fb->draw_line(deltax+125,57+deltay,deltax+134,57+deltay,0,0,150);
+		fb->draw_line(deltax+134,57+deltay,deltax+110,21+deltay,0,0,150);
+		fb->draw_line(deltax+110,21+deltay,deltax+112,11+deltay,0,0,150);
+		fb->draw_line(deltax+112,11+deltay,deltax+154,9+deltay,0,0,150);
+		fb->draw_line(deltax+154,9+deltay,deltax+167,28+deltay,0,0,150);
+		fb->draw_line(deltax+167,28+deltay,deltax+176,28+deltay,0,0,150);
+		fb->draw_line(deltax+176,28+deltay,deltax+169,11+deltay,0,0,150);
+		fb->draw_line(deltax+169,11+deltay,deltax+169,-11+deltay,0,0,150);
+		fb->draw_line(deltax+169,-11+deltay,deltax+176,-28+deltay,0,0,150);
+		fb->draw_line(deltax+176,-28+deltay,deltax+167,-28+deltay,0,0,150);
+		fb->draw_line(deltax+167,-28+deltay,deltax+154,-9+deltay,0,0,150);
+		fb->draw_line(deltax+154,-9+deltay,deltax+112,-11+deltay,0,0,150);
+		fb->draw_line(deltax+112,-11+deltay,deltax+110,-21+deltay,0,0,150);
+		fb->draw_line(deltax+110,-21+deltay,deltax+134,-57+deltay,0,0,150);
+		fb->draw_line(deltax+134,-57+deltay,deltax+125,-57+deltay,0,0,150);
+		fb->draw_line(deltax+125,-57+deltay,deltax+81,-11+deltay,0,0,150);
+		fb->draw_line(deltax+81,-11+deltay,deltax+16,-11+deltay,0,0,150);
+		fb->draw_line(deltax+16,-11+deltay,deltax+0,-3+deltay,0,0,150);
+		fb->draw_line(deltax+0,3+deltay,deltax+0,0+deltay,0,0,150);
+		fb->draw_line(deltax+0,-3+deltay,deltax+0,0+deltay,0,0,150);
+		fb->flood_fill(deltax+90,deltay,0,0,150);
+		
+		usleep(10000);
+
+		fb->reset_fill(deltax+90,deltay,0,0,0);
+		
+		deltax = deltax-3;
+		if (deltax <= 0) {
+			deltax = 1100;
+			n++;
+		}
+	}
+
+
 	// printf("%i\n", fb->isBlack(150,150));
 	// printf("%i\n", fb->isBlack(100,100));
 	// printf("%i\n", fb->isBlack(400,500));
